71e8cab1d6311442a224cad87daca9eed621403f
JiaZe <920687534@qq.com>
2022-04-20 13:03:04 +0800

    Using mode to reuse this widget.

--- qt_src/golgi_details.py
+++ qt_src/golgi_details.py
@@ -19,17 +19,21 @@ class GolgiDetailWidget(QWidget):
 
     def __init__(self, crop_golgi, giantin_mask, giantin_pred, param_dict):
         super().__init__()
+        self.setObjectName(window_name)
         self.ui = Ui_Golgi_details()
         self.ui.setupUi(self)
+        self.mode = mode
 
         self.crop_golgi = crop_golgi
-        self.giantin_mask = giantin_mask
-        self.giantin_pred = giantin_pred
-        self.giantin_channel = param_dict["param_giantin_channel"]
-        self.blank_channel = param_dict["param_blank_channel"]
-        self.overlapping = param_dict["param_giantin_overlap"]
-        self.giantin_possibility_threshold = param_dict["param_giantin_threshold"]
-        self.min_giantin_area = param_dict["param_giantin_area_threshold"]
+        if self.mode == 1:
+            self.ui.btn_save.setText("Save")
+            self.giantin_mask = giantin_mask
+            self.giantin_pred = giantin_pred
+            self.giantin_channel = param_dict["param_giantin_channel"]
+            self.blank_channel = param_dict["param_blank_channel"]
+            self.overlapping = param_dict["param_giantin_overlap"]
+            self.giantin_possibility_threshold = param_dict["param_giantin_threshold"]
+            self.min_giantin_area = param_dict["param_giantin_area_threshold"]
 
         self.new_shifted_golgi = None
         self.new_crop_golgi = None
@@ -44,8 +48,13 @@ class GolgiDetailWidget(QWidget):
         self.ui.btn_check.clicked.connect(self.check_handler)
         self.ui.btn_save.clicked.connect(lambda: self.save_signal.emit(0))
 
-        self.ui.btn_check.setDisabled(True)
-        self.ui.btn_save.setDisabled(True)
+            self.ui.btn_check.setDisabled(True)
+            self.ui.btn_save.setDisabled(True)
+        elif self.mode == 2:
+            self.ui.btn_save.setText("Save plots")
+            self.hide_widget_for_averaged()
+            self.ui.btn_save.clicked.connect(lambda: self.save_averaged_result())
+            # self.show_averaged_w_plot(self.crop_golgi)
 
     def sub_handler(self):
         if self.ui.sub_value.text() == "":
@@ -154,9 +163,86 @@ class GolgiDetailWidget(QWidget):
             if cur_widget is not None:
                 plotLayout.replaceWidget(cur_widget, static_canvas)
 
-# if __name__ == '__main__':
-#     data = pd.read_csv("../try/try.csv")
-#     app = QApplication(sys.argv)
-#     window = GolgiDetailWidget(np.array([data, data, data]).transpose((1, 2, 0)), data, giantin_channel=0)
-#     window.show()
-#     sys.exit(app.exec())
+    # for averaged data
+    def hide_widget_for_averaged(self):
+        self.ui.btn_sub_c1.setVisible(False)
+        self.ui.btn_sub_c2.setVisible(False)
+        self.ui.btn_sub_c3.setVisible(False)
+        self.ui.sub_value_c1.setVisible(False)
+        self.ui.sub_value_c2.setVisible(False)
+        self.ui.sub_value_c3.setVisible(False)
+        self.ui.label_c1.setVisible(False)
+        self.ui.label_c2.setVisible(False)
+        self.ui.label_c3.setVisible(False)
+        self.ui.btn_check.setVisible(False)
+
+    def show_averaged_w_plot(self, averaged_golgi):
+        num_channel = averaged_golgi.shape[-1]
+        columns = num_channel + 1
+        rows = 2
+        static_canvas = FigureCanvas(Figure(figsize=(3 * columns, 1 * rows)))
+        subplot_axes = static_canvas.figure.subplots(rows, columns)
+        static_canvas.figure.tight_layout(pad=0.6)
+
+        golgi_x_axis_labels = np.arange(0, 701, 350)
+        golgi_y_axis_labels = np.arange(700, -1, -350)
+        font_size = 9
+
+        # get plot data
+        radial_mean_intensity_df_list, radius_list = cal_radial_mean_intensity(averaged_golgi)
+
+        for j in range(columns - 1):
+            # show golgi
+            golgi_axes = subplot_axes[0][j]
+            golgi_axes.set_title("Channel {}".format(j + 1))
+            img_ = golgi_axes.imshow(averaged_golgi[:, :, j])
+            cbar = static_canvas.figure.colorbar(img_, ax=golgi_axes)
+
+            golgi_axes.set_xlim(np.min(golgi_x_axis_labels), np.max(golgi_x_axis_labels))
+            golgi_axes.set_ylim(np.max(golgi_y_axis_labels), np.min(golgi_y_axis_labels))
+            golgi_axes.set_xticks(golgi_x_axis_labels)
+            golgi_axes.set_yticks(golgi_y_axis_labels)
+
+            for t in cbar.ax.get_yticklabels():
+                t.set_fontsize(font_size)
+
+            for label in (golgi_axes.get_xticklabels() + golgi_axes.get_yticklabels()):
+                label.set_fontsize(font_size)
+
+            # show plot
+            plot_axes = subplot_axes[1][j]
+            plot_axes.plot(radial_mean_intensity_df_list[j]["normalized_mean_intensity"])
+            for label in (plot_axes.get_xticklabels() + plot_axes.get_yticklabels()):
+                label.set_fontsize(font_size)
+
+        subplot_axes[0][-1].set_title("Merge")
+        merge_img = averaged_golgi / np.amax(averaged_golgi, axis=(0, 1))
+        subplot_axes[0][-1].imshow(merge_img)
+
+        subplot_axes[1][-1].plot(radial_mean_intensity_df_list[0]["normalized_mean_intensity"])
+        subplot_axes[1][-1].plot(radial_mean_intensity_df_list[1]["normalized_mean_intensity"])
+        subplot_axes[1][-1].plot(radial_mean_intensity_df_list[2]["normalized_mean_intensity"])
+
+        for axes in subplot_axes[1]:
+            axes.set_xlabel("Distance from center")
+            axes.set_ylabel("Radial mean intensity")
+
+        plotLayout = QVBoxLayout()
+        plotLayout.setContentsMargins(2, 2, 2, 2)
+        plotLayout.addWidget(static_canvas)
+        self.ui.golgi_content_widget.setLayout(plotLayout)
+
+    def save_averaged_result(self):
+        ...
+
+
+if __name__ == '__main__':
+    param_dict = {"param_giantin_area_threshold": 150, "param_giantin_threshold": 0.6, "param_giantin_overlap": True,
+                  "param_blank_channel": -1, "param_giantin_channel": 0}
+    data = pd.read_csv("../try/try.csv")
+    app = QApplication(sys.argv)
+    # window = GolgiDetailWidget("Averaged golgi mini-stacks", mode=2)
+    window = GolgiDetailWidget("Averaged golgi mini-stacks", mode=1, crop_golgi=np.dstack([data, data, data]),
+                               param_dict=param_dict)
+    window.show()
+    sys.exit(app.exec())
